2fe253ad75da7bfae8eb211b142d26791a39d4de
RajatKumarMahata <rajatkumarmahata7@gmail.com>
2022-10-27 09:45:32 +0530

    done

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -1,7 +1,7 @@
 function formatter(){
     let a= "                 FUNCTION UP                       "
-    let b= "I Am AaKaSH GhOsH"
-    let c= "aakash ghosh"
+    let b= "I Am Rajat"
+    let c= "rajat"
     console.log(a.trim())
     console.log(b.toLowerCase())
     console.log(c.toUpperCase())
